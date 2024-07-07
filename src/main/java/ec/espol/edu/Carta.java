14df6845d65578c13815473d9e0735420318fc63
ArielAT123 <126928333+ArielAT123@users.noreply.github.com>
2024-07-07 15:29:28 -0500

    La clase Carta:
    Es una clase abstracta que sirve como base para todas las cartas del juego de UNO.
    Tiene un atributo protegido color.
    Incluye un constructor por defecto y un constructor que inicializa el color (siempre que no sea el color 'N').
    Proporciona un método getColor para obtener el color de la carta.
    Define un método abstracto puedeJugar que deberá ser implementado por las subclases para definir las reglas de juego.
    Sobrescribe el método toString para una representación básica de la carta.

---
+++ src/main/java/ec/espol/edu/Carta.java
@@ -0,0 +1,21 @@
+package ec.espol.edu;
+
+public abstract class Carta {
+    protected Color color;
+    public Carta(){}
+    public Carta(Color color) {
+        if(color!=Color.N)
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public abstract boolean puedeJugar(Carta otraCarta);
+
+    @Override
+    public String toString() {
+        return " Carta[color=" + color + "]";
+    }
+}
